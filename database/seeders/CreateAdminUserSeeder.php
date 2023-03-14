d4099ad90d6676f82281d6922bdd2fc1414072cd
Yogi Perdana <yogiperdana3@gmail.com>
2023-03-14 12:15:10 +0700

    Implement adminlte ui and auth

---
+++ database/seeders/CreateAdminUserSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Support\Facades\Hash;
+
+class CreateAdminUserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        return User::create([
+            'name' => 'admin',
+            'email' => 'admin@gmail.com',
+            'password' => Hash::make('password'),
+        ]);
+    }
+}

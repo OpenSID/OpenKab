bfa93ba352f2e82f97d083bd70386d1e800d1935
Agung Sugiarto <sugiartoagung92@gmail.com>
2023-04-03 13:24:03 +0700

    check column

--- app/Models/Scopes/ConfigIdScope.php
+++ app/Models/Scopes/ConfigIdScope.php
@@ -6,6 +6,7 @@ use App\Models\Config;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Scope;
+use Illuminate\Support\Facades\Schema;
 
 class ConfigIdScope implements Scope
 {
@@ -14,6 +15,10 @@ class ConfigIdScope implements Scope
      */
     public function apply(Builder $builder, Model $model)
     {
+        if (! Schema::setConnection($model->getConnection())->hasColumn($model->getTable(), 'config_id')) {
+            return;
+        }
+
         // from web request
         if (session()->has('desa')) {
             return $builder->where("{$model->getTable()}.config_id", session('desa.id'));

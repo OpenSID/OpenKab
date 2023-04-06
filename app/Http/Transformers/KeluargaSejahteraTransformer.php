9b6bab348725c934c17a7da418c381b0b6b9020d
Yogi Perdana <yogiperdana3@gmail.com>
2023-04-06 10:59:23 +0700

    Sesuaikan statistik

--- app/Http/Transformers/KeluargaSejahteraTransformer.php
+++ app/Http/Transformers/KeluargaSejahteraTransformer.php
@@ -9,6 +9,17 @@ class KeluargaSejahteraTransformer extends TransformerAbstract
 {
     public function transform(KeluargaSejahtera $keluargaSejahtera)
     {
-        return $keluargaSejahtera->toArray();
+        return [
+            'id'                   => $keluargaSejahtera->id,
+            'nama'                 => $keluargaSejahtera->nama,
+            'jumlah'               => $keluargaSejahtera->statistik[0]['jumlah'],
+            'persentase_jumlah'    => $keluargaSejahtera->statistik[0]['persentase_jumlah'],
+            'laki_laki'            => $keluargaSejahtera->statistik[0]['laki_laki'],
+            'persentase_laki_laki' => $keluargaSejahtera->statistik[0]['persentase_laki_laki'],
+            'perempuan'            => $keluargaSejahtera->statistik[0]['perempuan'],
+            'persentase_perempuan' => $keluargaSejahtera->statistik[0]['persentase_perempuan'],
+            'at'         => $keluargaSejahtera->at,
+            'statistik'         => $keluargaSejahtera->statistik,
+        ];
     }
 }
--- app/Models/KeluargaSejahtera.php
+++ app/Models/KeluargaSejahtera.php
@@ -39,23 +39,45 @@ class KeluargaSejahtera extends Model
 
     private function getStatistik($id)
     {
-        $keluarga = $this->getKeluarga($id);
-        $total  = $this->getTotal();
+        $keluarga       = $this->getKeluarga($id);
+        $total_keluarga = $this->getTotalKeluarga();
+        $total          = $this->getTotal();
 
         return [
             [
                 'jumlah'    => $keluarga->jumlah,
+                'laki_laki' => $keluarga->laki_laki,
+                'perempuan' => $keluarga->perempuan,
                 'persentase_jumlah' => $total->jumlah > 0 ? $keluarga->jumlah / $total->jumlah * 100 : 0,
+                'persentase_laki_laki' => $total->laki_laki > 0 ? $keluarga->laki_laki / $total->laki_laki * 100 : 0,
+                'persentase_perempuan' => $total->perempuan > 0 ? $keluarga->perempuan / $total->perempuan * 100 : 0,
                 'nama'      => 'Keluarga',
             ],
             [
-                'jumlah'    => $total->jumlah - $keluarga->jumlah,
-                'persentase_jumlah' => $total->jumlah > 0 ? ($total->jumlah - $keluarga->jumlah) / $total->jumlah * 100 : 0,
+                'jumlah'    => $total_keluarga->jumlah,
+                'laki_laki' => $total_keluarga->laki_laki,
+                'perempuan' => $total_keluarga->perempuan,
+                'persentase_jumlah' => $total->jumlah > 0 ? $total_keluarga->jumlah / $total->jumlah * 100 : 0,
+                'persentase_laki_laki' => $total->laki_laki > 0 ? $total_keluarga->laki_laki / $total->laki_laki * 100 : 0,
+                'persentase_perempuan' => $total->perempuan > 0 ? $total_keluarga->perempuan / $total->perempuan * 100 : 0,
+                'nama'      => 'Jumlah',
+            ],
+            [
+                'jumlah'    => $total->jumlah - $total_keluarga->jumlah,
+                'laki_laki' => $total->laki_laki - $total_keluarga->laki_laki,
+                'perempuan' => $total->perempuan - $total_keluarga->perempuan,
+                'persentase_jumlah' => $total->jumlah > 0 ? ($total->jumlah - $total_keluarga->jumlah) / $total->jumlah * 100 : 0,
+                'persentase_laki_laki' => $total->laki_laki > 0 ? ($total->laki_laki - $total_keluarga->laki_laki) / $total->laki_laki * 100 : 0,
+                'persentase_perempuan' => $total->perempuan > 0 ? ($total->perempuan - $total_keluarga->perempuan) / $total->perempuan * 100 : 0,
                 'nama'      => 'Belum Mengisi',
             ],
             [
                 'jumlah'    => $total->jumlah,
+                'laki_laki' => $total->laki_laki,
+                'perempuan' => $total->perempuan,
                 'persentase_jumlah' => 100,
+                'persentase_laki_laki' => 100,
+                'persentase_perempuan' => 100,
                 'nama'      => 'Total',
             ],
         ];
@@ -63,18 +85,34 @@ class KeluargaSejahtera extends Model
 
     private function getKeluarga($id)
     {
-        $query = DB::connection($this->connection)->table('tweb_keluarga_sejahtera')
+        return DB::connection($this->connection)->table('tweb_keluarga_sejahtera')
             ->selectRaw('COUNT(tweb_keluarga.id) AS jumlah')
+            ->selectRaw('COUNT(CASE WHEN tweb_penduduk.sex = 1 THEN tweb_penduduk.id END) AS laki_laki')
+            ->selectRaw('COUNT(CASE WHEN tweb_penduduk.sex = 2 THEN tweb_penduduk.id END) AS perempuan')
             ->where('tweb_keluarga_sejahtera.id', $id)
-            ->join('tweb_keluarga', 'tweb_keluarga.kelas_sosial', '=', 'tweb_keluarga_sejahtera.id', 'left');
-
-        return $query->first();
+            ->join('tweb_keluarga', 'tweb_keluarga.kelas_sosial', '=', 'tweb_keluarga_sejahtera.id', 'left')
+            ->join('tweb_penduduk', 'tweb_penduduk.id', '=', 'tweb_keluarga.nik_kepala', 'left')
+            ->first();
     }
 
     private function getTotal()
     {
-        $query = DB::connection($this->connection)->table('tweb_keluarga');
+        return DB::connection($this->connection)->table('tweb_keluarga')
+            ->selectRaw('COUNT(tweb_keluarga.id) AS jumlah')
+            ->selectRaw('COUNT(CASE WHEN tweb_penduduk.sex = 1 THEN tweb_penduduk.id END) AS laki_laki')
+            ->selectRaw('COUNT(CASE WHEN tweb_penduduk.sex = 2 THEN tweb_penduduk.id END) AS perempuan')
+            ->join('tweb_penduduk', 'tweb_penduduk.id', '=', 'tweb_keluarga.nik_kepala', 'left')
+            ->first();
+    }
 
-        return $query->selectRaw('COUNT(tweb_keluarga.id) AS jumlah')->first();
+    private function getTotalKeluarga()
+    {
+        return DB::connection($this->connection)->table('tweb_keluarga')
+            ->selectRaw('COUNT(tweb_keluarga.id) AS jumlah')
+            ->selectRaw('COUNT(CASE WHEN tweb_penduduk.sex = 1 THEN tweb_penduduk.id END) AS laki_laki')
+            ->selectRaw('COUNT(CASE WHEN tweb_penduduk.sex = 2 THEN tweb_penduduk.id END) AS perempuan')
+            ->join('tweb_penduduk', 'tweb_penduduk.id', '=', 'tweb_keluarga.nik_kepala', 'left')
+            ->where('kelas_sosial', '!=', null)
+            ->first();
     }
 }

993ceb25ccc907ad6d3b1687d00adbd101e5ec3c
Andi Fahruddin Akas <andifahruddinakas@gmail.com>
2023-04-06 21:59:16 +0800

    ok

--- app/Http/Controllers/Api/RtmController.php
+++ app/Http/Controllers/Api/RtmController.php
@@ -21,6 +21,72 @@ class RtmController extends Controller
 
     public function grafik()
     {
-        return $this->fractal($this->statistik->getStatistik([], []), new StatistikTransformer(), 'grafik')->respond();
+        $footer = [
+            [
+                'nama' => 'JUMLAH',
+                'jumlah' => 92,
+                'laki_laki' => 45,
+                'perempuan' => 47,
+            ],
+            [
+                'nama' => 'BELUM MENGISI',
+                'jumlah' => 0,
+                'laki_laki' => 0,
+                'perempuan' => 0,
+            ],
+            [
+                'nama' => 'TOTAL',
+                'jumlah' => 92,
+                'laki_laki' => 45,
+                'perempuan' => 47,
+            ]
+        ];
+
+        $header = [
+            [
+                'id' => 1,
+                'nama' => 'ISLAM',
+                'laki_laki' => 41,
+                'perempuan' => 43,
+            ],
+            [
+                'id' => 2,
+                'nama' => 'KRISTEN',
+                'laki_laki' => 0,
+                'perempuan' => 0,
+            ],
+            [
+                'id' => 3,
+                'nama' => 'KATHOLIK',
+                'laki_laki' => 0,
+                'perempuan' => 0,
+            ],
+            [
+                'id' => 4,
+                'nama' => 'HINDU',
+                'laki_laki' => 3,
+                'perempuan' => 4,
+            ],
+            [
+                'id' => 5,
+                'nama' => 'BUDHA',
+                'laki_laki' => 1,
+                'perempuan' => 0,
+            ],
+            [
+                'id' => 6,
+                'nama' => 'KHONGHUCU',
+                'laki_laki' => 0,
+                'perempuan' => 0,
+            ],
+            [
+                'id' => 7,
+                'nama' => 'KEPERCAYAAN TERHADAP TUHAN YME / LAINNYA',
+                'laki_laki' => 0,
+                'perempuan' => 0,
+            ]
+        ];
+
+        return $this->fractal($this->statistik->getStatistik($header, $footer), new StatistikTransformer(), 'grafik')->respond();
     }
 }
--- app/Http/Repository/StatistikRepository.php
+++ app/Http/Repository/StatistikRepository.php
@@ -6,82 +6,20 @@ class StatistikRepository
 {
     public function getStatistik(array $header = [], array $footer = [])
     {
-        $footer = [
-            [
-                'nama' => 'JUMLAH',
-                'jumlah' => 92,
-                'laki_laki' => 45,
-                'perempuan' => 47,
-            ],
-            [
-                'nama' => 'BELUM MENGISI',
-                'jumlah' => 0,
-                'laki_laki' => 0,
-                'perempuan' => 0,
-            ],
-            [
-                'nama' => 'TOTAL',
-                'jumlah' => 92,
-                'laki_laki' => 45,
-                'perempuan' => 47,
-            ]
-        ];
-
-        $header = [
-            [
-                'id' => 1,
-                'nama' => 'ISLAM',
-                'laki_laki' => 41,
-                'perempuan' => 43,
-            ],
-            [
-                'id' => 2,
-                'nama' => 'KRISTEN',
-                'laki_laki' => 0,
-                'perempuan' => 0,
-            ],
-            [
-                'id' => 3,
-                'nama' => 'KATHOLIK',
-                'laki_laki' => 0,
-                'perempuan' => 0,
-            ],
-            [
-                'id' => 4,
-                'nama' => 'HINDU',
-                'laki_laki' => 3,
-                'perempuan' => 4,
-            ],
-            [
-                'id' => 5,
-                'nama' => 'BUDHA',
-                'laki_laki' => 1,
-                'perempuan' => 0,
-            ],
-            [
-                'id' => 6,
-                'nama' => 'KHONGHUCU',
-                'laki_laki' => 0,
-                'perempuan' => 0,
-            ],
-            [
-                'id' => 7,
-                'nama' => 'KEPERCAYAAN TERHADAP TUHAN YME / LAINNYA',
-                'laki_laki' => 0,
-                'perempuan' => 0,
-            ]
-        ];
-
         $setFooter = $this->getHitungFooter($footer);
 
-        $setHeader = $this->getHitungHeader($header, $setFooter[0]['jumlah']);
+        if (count($header) > 0) {
+            $setFooter = collect($setFooter)->map(function ($item, $key) use ($header) {
+                $item['id'] = $key + 1 + count($header);
+                return $item;
+            });
 
-        // $setHitung = collect($hitung)->map(function ($item, $key) {
-        //     $item['id'] = $key + 1;
-        //     return $item;
-        // });
+            $setHeader = $this->getHitungHeader($header, $setFooter[0]['jumlah']);
 
-        return collect($setHeader)->merge($setFooter)->toArray();
+            return [... $setHeader, ... $setFooter];
+        }
+
+        return $setFooter;
     }
 
     private function getHitungHeader(array $kategori = [], int $total = 0)

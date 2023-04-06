2191b9d41f4e36edbe924607c14da0c422d5951a
Andi Fahruddin Akas <andifahruddinakas@gmail.com>
2023-04-06 16:57:08 +0800

    fdd

--- app/Http/Controllers/Api/RtmController.php
+++ app/Http/Controllers/Api/RtmController.php
@@ -2,8 +2,12 @@
 
 namespace App\Http\Controllers\Api;
 
+use League\Fractal\Manager;
+use Spatie\Fractal\Fractal;
 use App\Http\Repository\RtmRepository;
+use League\Fractal\Resource\Collection;
 use App\Http\Controllers\Api\Controller;
+use Spatie\Fractalistic\ArraySerializer;
 use App\Http\Transformers\RtmTransformer;
 use App\Http\Transformers\BantuanTransformer;
 use App\Http\Transformers\GrafikRtmTransformer;
@@ -16,19 +20,26 @@ class RtmController extends Controller
 
     public function index()
     {
-        return $this->fractal($this->rtm->listRtm(), new RtmTransformer(), 'rtm')->respond();
+        // return $this->fractal($this->rtm->listRtm(), new RtmTransformer(), 'rtm')->respond();
     }
 
     public function grafik()
     {
-        return response()->json([
-            'id' => 1,
-            'sasaran' => '1',
-        ]);
+        $data = [
+            [
+                'id' => 2,
+                'nama' => 'John Doe',
+                'email' => 'john@example.com',
+                'phone' => '555-1234',
+            ],
+            [
+                'id' => 3,
+                'nama' => 'Jane Doe',
+                'email' => 'jane@example.com',
+                'phone' => '555-5678',
+            ],
+        ];
 
-        // return $this->fractal([
-        //     'id' => 1,
-        //     'sasaran' => '1',
-        // ], new GrafikRtmTransformer(), 'grafik')->respond();
+        return $this->fractal($data, new GrafikRtmTransformer(), 'grafik')->respond();
     }
 }
--- app/Http/Transformers/GrafikRtmTransformer.php
+++ app/Http/Transformers/GrafikRtmTransformer.php
@@ -3,90 +3,93 @@
 namespace App\Http\Transformers;
 
 use App\Models\Rtm;
+use Fractal\Resource\Collection;
+use Illuminate\Support\Facades\DB;
 use League\Fractal\TransformerAbstract;
 
 class GrafikRtmTransformer extends TransformerAbstract
 {
     /** {@inheritdoc} */
-    // protected $dbConnection;
+    protected $connection = 'openkab';
 
-    // /**
-    //  * constract
-    //  */
-    // public function __construct()
-    // {
-    //     $this->dbConnection = DB::connection($this->connection);
-    // }
+    /** {@inheritdoc} */
+    protected $dbConnection;
 
-    public function transform()
+    /**
+     * constract
+     */
+    public function __construct()
     {
-        return [
-            'id' => 1,
-            'sasaran' => '1',
-        ];
-
-        // $this->getStatistikAttribute();
+        $this->dbConnection = DB::connection($this->connection);
     }
 
-    // public function getStatistikAttribute()
-    // {
-    //     return $this->getStatistik();
-    // }
+    public function transform($user)
+    {
+        return [
+            'id' => $user['id'],
+            'nama' => [
+                'sy' => $user['nama'],
+            ],
+        ];
+    }
 
-    // private function getStatistik()
-    // {
-    //     return null;
+    private function getStatistik()
+    {
+        return null;
 
-    //     $peserta = $this->getPeserta();
-    //     $total  = $this->getTotal();
+        $peserta = $this->getPeserta();
+        $total  = $this->getTotal();
 
-    //     return [
-    //         [
-    //             'jumlah'    => $peserta->jumlah,
-    //             'laki_laki' => $peserta->laki_laki,
-    //             'perempuan' => $peserta->perempuan,
-    //             'persentase_jumlah' => $total->jumlah > 0 ? $peserta->jumlah / $total->jumlah * 100 : 0,
-    //             'persentase_laki_laki' => $total->laki_laki > 0 ? $peserta->laki_laki / $total->laki_laki * 100 : 0,
-    //             'persentase_perempuan' => $total->perempuan > 0 ? $peserta->perempuan / $total->perempuan * 100 : 0,
-    //             'nama'      => 'Peserta',
-    //         ],
-    //         [
-    //             'jumlah'    => $total->jumlah - $peserta->jumlah,
-    //             'laki_laki' => $total->laki_laki - $peserta->laki_laki,
-    //             'perempuan' => $total->perempuan - $peserta->perempuan,
-    //             'persentase_jumlah' => $total->jumlah > 0 ? ($total->jumlah - $peserta->jumlah) / $total->jumlah * 100 : 0,
-    //             'persentase_laki_laki' => $total->laki_laki > 0 ? ($total->laki_laki - $peserta->laki_laki) / $total->laki_laki * 100 : 0,
-    //             'persentase_perempuan' => $total->perempuan > 0 ? ($total->perempuan - $peserta->perempuan) / $total->perempuan * 100 : 0,
-    //             'nama'      => 'Bukan Peserta',
-    //         ],
-    //         [
-    //             'jumlah'    => $total->jumlah,
-    //             'laki_laki' => $total->laki_laki,
-    //             'perempuan' => $total->perempuan,
-    //             'persentase_jumlah' => 100,
-    //             'persentase_laki_laki' => 100,
-    //             'persentase_perempuan' => 100,
-    //             'nama'      => 'Total',
-    //         ],
-    //     ];
-    // }
+        return [
+            [
+                'id'        => 1,
+                'jumlah'    => $peserta->jumlah,
+                'laki_laki' => $peserta->laki_laki,
+                'perempuan' => $peserta->perempuan,
+                'persentase_jumlah' => $total->jumlah > 0 ? $peserta->jumlah / $total->jumlah * 100 : 0,
+                'persentase_laki_laki' => $total->laki_laki > 0 ? $peserta->laki_laki / $total->laki_laki * 100 : 0,
+                'persentase_perempuan' => $total->perempuan > 0 ? $peserta->perempuan / $total->perempuan * 100 : 0,
+                'nama'      => 'Peserta',
+            ],
+            [
+                'id'        => 2,
+                'jumlah'    => $total->jumlah - $peserta->jumlah,
+                'laki_laki' => $total->laki_laki - $peserta->laki_laki,
+                'perempuan' => $total->perempuan - $peserta->perempuan,
+                'persentase_jumlah' => $total->jumlah > 0 ? ($total->jumlah - $peserta->jumlah) / $total->jumlah * 100 : 0,
+                'persentase_laki_laki' => $total->laki_laki > 0 ? ($total->laki_laki - $peserta->laki_laki) / $total->laki_laki * 100 : 0,
+                'persentase_perempuan' => $total->perempuan > 0 ? ($total->perempuan - $peserta->perempuan) / $total->perempuan * 100 : 0,
+                'nama'      => 'Bukan Peserta',
+            ],
+            [
+                'id'        => 3,
+                'jumlah'    => $total->jumlah,
+                'laki_laki' => $total->laki_laki,
+                'perempuan' => $total->perempuan,
+                'persentase_jumlah' => 100,
+                'persentase_laki_laki' => 100,
+                'persentase_perempuan' => 100,
+                'nama'      => 'Total',
+            ],
+        ];
+    }
 
-    // private function getPeserta()
-    // {
-    //     return $this->getRtmRaw()->where('tweb_rtm.bdt', '=', null)->get();
-    // }
+    private function getPeserta()
+    {
+        return $this->getRtmRaw()->where('tweb_rtm.bdt', '=', null)->get();
+    }
 
-    // private function getTotal()
-    // {
-    //     return $this->getRtmRaw()->get();
-    // }
+    private function getTotal()
+    {
+        return $this->getRtmRaw()->get();
+    }
 
-    // private function getRtmRaw()
-    // {
-    //     return $this->dbConnection->table('tweb_rtm')
-    //         ->selectRaw('COUNT(tweb_penduduk.id) AS jumlah')
-    //         ->selectRaw('COUNT(CASE WHEN tweb_penduduk.sex = 1 THEN tweb_penduduk.id END) AS laki_laki')
-    //         ->selectRaw('COUNT(CASE WHEN tweb_penduduk.sex = 2 THEN tweb_penduduk.id END) AS perempuan')
-    //         ->join('tweb_penduduk', 'tweb_penduduk.id', '=', 'tweb_rtm.nik_kepala', 'left');
-    // }
+    private function getRtmRaw()
+    {
+        return $this->dbConnection->table('tweb_rtm')
+            ->selectRaw('COUNT(tweb_penduduk.id) AS jumlah')
+            ->selectRaw('COUNT(CASE WHEN tweb_penduduk.sex = 1 THEN tweb_penduduk.id END) AS laki_laki')
+            ->selectRaw('COUNT(CASE WHEN tweb_penduduk.sex = 2 THEN tweb_penduduk.id END) AS perempuan')
+            ->join('tweb_penduduk', 'tweb_penduduk.id', '=', 'tweb_rtm.nik_kepala', 'left');
+    }
 }

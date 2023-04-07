8d2c942c243dc3886611fe6fdf499fa48d824dac
Andi Fahruddin Akas <andifahruddinakas@gmail.com>
2023-04-08 03:20:23 +0800

    Update index.blade.php

--- resources/views/statistik/keluarga/index.blade.php
+++ resources/views/statistik/keluarga/index.blade.php
@@ -31,7 +31,6 @@
                                 </a>
                             </li>
                         @endforeach
-
                     </ul>
                 </div>
             </div>

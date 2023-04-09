c3ffe18a9b3658151d1f605630950785e3bd9c76
Firliani Fauziah <firlianif@gmail.com>
2023-04-09 09:59:56 +0700

    Statistik penduduk penyandang cacat (#101)
    Co-authored-by: Andi Fahruddin Akas <andifahruddinakas@gmail.com>

--- app/Models/Penduduk.php
+++ app/Models/Penduduk.php
@@ -32,7 +32,7 @@ class Penduduk extends Model
         'warga-negara' => 'Warga Negara',
         'status-penduduk' => 'Status Penduduk',
         // 'darah' => 'Golongan Darah',
-        // 'cacat' => 'Penyandang Cacat',
+        'penyandang-cacat' => 'Penyandang Cacat',
         'penyakit-menahun' => 'Penyakit Menahun',
         // 'kb' => 'Aseptor KB',
         // 'ktp' => 'Kepemilikan KTP',

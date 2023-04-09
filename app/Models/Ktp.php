d03b8f0eb2a062e9ceacabe629c7bb4893109205
FirlianiF <firlianif@gmail.com>
2023-04-09 13:54:27 +0700

    update

--- app/Models/Ktp.php
+++ app/Models/Ktp.php
@@ -26,7 +26,7 @@ class Ktp extends Model
     {
         $where = "((DATE_FORMAT(FROM_DAYS(TO_DAYS(NOW()) - TO_DAYS(tanggallahir)), '%Y')+0)>=17 OR (status_kawin IS NOT NULL AND status_kawin <> 1)) AND status_rekam = status_rekam ";
         return $query
-            ->select(['id', 'nama'])
+            ->select('*')
             ->selectRaw("(SELECT COUNT(tweb_penduduk.id) FROM tweb_penduduk WHERE tweb_penduduk.`sex` = '1' AND tweb_penduduk.`status_dasar` = 1 AND $where) as laki_laki")
             ->selectRaw("(SELECT COUNT(tweb_penduduk.id) FROM tweb_penduduk WHERE tweb_penduduk.`sex` = '2' AND tweb_penduduk.`status_dasar` = 1 AND $where) as perempuan");
     }

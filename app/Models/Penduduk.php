76aaf40b99b64673fb2fbb791d89dc70eeec76a6
FirlianiF <firlianif@gmail.com>
2023-04-09 09:10:14 +0700

    kawin

--- app/Models/Penduduk.php
+++ app/Models/Penduduk.php
@@ -27,7 +27,7 @@ class Penduduk extends Model
         'pendidikan-dalam-kk' => 'Pendidikan Dalam KK',
         'pendidikan-sedang-ditempuh' => 'Pendidikan Sedang Ditempuh',
         // 'kerja' => 'Pekerjaan',
-        // 'kawin' => 'Status Perkawinan',
+         'status-perkawinan' => 'Status Perkawinan',
         // 'agama' => 'Agama',
         // 'jk' => 'Jenis Kelamin',
         // 'wn' => 'Warga Negara',

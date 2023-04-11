5d0a70c971a45ace97580582cfafa9d72913e474
Andi Fahruddin Akas <andifahruddinakas@gmail.com>
2023-04-11 16:23:19 +0800

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -42,10 +42,12 @@ Route::middleware('auth')->group(function () {
     });
 
 
-    Route::controller(\App\Http\Controllers\BantuanController::class)->prefix('bantuan')->group(function () {
-        Route::get('/', 'index');
-        Route::get('/{id}', 'show');
-    });
+    Route::controller(\App\Http\Controllers\BantuanController::class)
+        ->prefix('bantuan')
+        ->group(function () {
+            Route::get('/', 'index');
+            Route::get('/{id}', 'show');
+        });
 
     // Statistik
     Route::controller(\App\Http\Controllers\StatistikController::class)

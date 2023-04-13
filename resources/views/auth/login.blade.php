671d1ef479fb109ca20bd02f16a15a40d85bf612
Andi Fahruddin Akas <andifahruddinakas@gmail.com>
2023-04-13 13:04:33 +0800

    hapus load asset tidak digunakan

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,21 +1,17 @@
 @extends('adminlte::auth.auth-page', ['auth_type' => 'login'])
 
-@section('adminlte_css_pre')
-    <link rel="stylesheet" href="{{ asset('vendor/icheck-bootstrap/icheck-bootstrap.min.css') }}">
-@stop
-
-@php( $login_url = View::getSection('login_url') ?? config('adminlte.login_url', 'login') )
-@php( $register_url = View::getSection('register_url') ?? config('adminlte.register_url', 'register') )
-@php( $password_reset_url = View::getSection('password_reset_url') ?? config('adminlte.password_reset_url', 'password/reset') )
+@php($login_url = View::getSection('login_url') ?? config('adminlte.login_url', 'login'))
+@php($register_url = View::getSection('register_url') ?? config('adminlte.register_url', 'register'))
+@php($password_reset_url = View::getSection('password_reset_url') ?? config('adminlte.password_reset_url', 'password/reset'))
 
 @if (config('adminlte.use_route_url', false))
-    @php( $login_url = $login_url ? route($login_url) : '' )
-    @php( $register_url = $register_url ? route($register_url) : '' )
-    @php( $password_reset_url = $password_reset_url ? route($password_reset_url) : '' )
+    @php($login_url = $login_url ? route($login_url) : '')
+    @php($register_url = $register_url ? route($register_url) : '')
+    @php($password_reset_url = $password_reset_url ? route($password_reset_url) : '')
 @else
-    @php( $login_url = $login_url ? url($login_url) : '' )
-    @php( $register_url = $register_url ? url($register_url) : '' )
-    @php( $password_reset_url = $password_reset_url ? url($password_reset_url) : '' )
+    @php($login_url = $login_url ? url($login_url) : '')
+    @php($register_url = $register_url ? url($register_url) : '')
+    @php($password_reset_url = $password_reset_url ? url($password_reset_url) : '')
 @endif
 
 @section('auth_header', __('adminlte::adminlte.login_message'))
@@ -24,10 +20,11 @@
     <form action="{{ $login_url }}" method="post">
         @csrf
 
-        {{-- Email field --}}
+        {{-- Email / Username field --}}
         <div class="input-group mb-3">
-            <input type="text" name="login" class="form-control {{ $errors->has('username') || $errors->has('email') ? ' is-invalid' : '' }}"
-                   value="{{ old('username') ?: old('email') }}" placeholder="Email atau nama pengguna" autofocus>
+            <input type="text" name="login"
+                class="form-control {{ $errors->has('username') || $errors->has('email') ? ' is-invalid' : '' }}"
+                value="{{ old('username') ?: old('email') }}" placeholder="Email atau nama pengguna" autofocus>
 
             <div class="input-group-append">
                 <div class="input-group-text">
@@ -45,7 +42,7 @@
         {{-- Password field --}}
         <div class="input-group mb-3">
             <input type="password" name="password" class="form-control @error('password') is-invalid @enderror"
-                   placeholder="{{ __('adminlte::adminlte.password') }}">
+                placeholder="{{ __('adminlte::adminlte.password') }}">
 
             <div class="input-group-append">
                 <div class="input-group-text">
@@ -73,7 +70,8 @@
             </div>
 
             <div class="col-5">
-                <button type=submit class="btn btn-block {{ config('adminlte.classes_auth_btn', 'btn-flat btn-primary') }}">
+                <button type=submit
+                    class="btn btn-block {{ config('adminlte.classes_auth_btn', 'btn-flat btn-primary') }}">
                     <span class="fas fa-sign-in-alt"></span>
                     {{ __('adminlte::adminlte.sign_in') }}
                 </button>
@@ -85,7 +83,7 @@
 
 @section('auth_footer')
     {{-- Password reset link --}}
-    @if($password_reset_url)
+    @if ($password_reset_url)
         <p class="my-0">
             <a href="{{ $password_reset_url }}">
                 {{ __('adminlte::adminlte.i_forgot_my_password') }}
@@ -94,7 +92,7 @@
     @endif
 
     {{-- Register link --}}
-    @if($register_url)
+    @if ($register_url)
         <p class="my-0">
             <a href="{{ $register_url }}">
                 {{ __('adminlte::adminlte.register_a_new_membership') }}

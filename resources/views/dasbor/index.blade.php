7e6318ee06ee849b11509ad9b215af9e6f605e38
Andi Fahruddin Akas <andifahruddinakas@gmail.com>
2023-04-17 12:09:39 +0800

    ok

--- resources/views/dasbor/index.blade.php
+++ resources/views/dasbor/index.blade.php
@@ -11,15 +11,38 @@
 
 @section('content')
     <x-adminlte-callout theme="warning">
-        Selamat datang di Dasbor Utama
-        {{ config('app.namaAplikasi') . ' ' . config('app.sebutanKab') . ' ' . config('app.namaKab') }}.
+        Selamat datang <b>{{ Auth::user()->username }}</b> di Dasbor Utama
+        <b>{{ config('app.namaAplikasi') . ' ' . config('app.sebutanKab') . ' ' . config('app.namaKab') }}</b>.
     </x-adminlte-callout>
+
+    <div class="row">
+        <div class="col-12 col-sm-6 col-md-3">
+            <x-adminlte-info-box id="penduduk" title="Penduduk" text="L : 2999 | P : 1999" icon="fas fa-lg fa-user"
+                icon-theme="blue" />
+        </div>
+
+        <div class="col-12 col-sm-6 col-md-3">
+            <x-adminlte-info-box id="keluarga" title="Keluarga" text="2991" icon="fas fa-lg fa-users" icon-theme="red" />
+        </div>
+
+        <div class="col-12 col-sm-6 col-md-3">
+            <x-adminlte-info-box id="rtm" title="RTM" text="221" icon="fas fa-lg fa-home" icon-theme="green" />
+        </div>
+
+        <div class="col-12 col-sm-6 col-md-3">
+            <x-adminlte-info-box id="bantuan" title="Bantuan" text="22" icon="fas fa-lg fa-handshake"
+                icon-theme="yellow" />
+        </div>
+    </div>
 @endsection
 
 @push('js')
     <script>
         $(document).ready(function() {
-            $('.progressive replace').progressiveImage();
+            $('#penduduk').find('.info-box-number').text('1');
+            $('#keluarga').find('.info-box-number').text('2');
+            $('#rtm').find('.info-box-number').text('3');
+            $('#bantuan').find('.info-box-number').text('4');
         });
     </script>
 @endpush

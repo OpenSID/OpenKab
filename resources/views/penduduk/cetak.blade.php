31600ff88f77b7a262f71e61f4116a33f0864253
Agung Sugiarto <sugiartoagung92@gmail.com>
2023-04-27 11:51:53 +0700

    cetak penduduk (#134)
    * cetak penduduk
    
    * Fix styling
    
    ---------
    
    Co-authored-by: agungsugiarto <agungsugiarto@users.noreply.github.com>
    Co-authored-by: Andi Fahruddin Akas <andifahruddinakas@gmail.com>

---
+++ resources/views/penduduk/cetak.blade.php
@@ -0,0 +1,128 @@
+<!DOCTYPE html
+    PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+
+<head>
+    <title>Data Statistik</title>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <meta name="robots" content="noindex">
+    <link rel="shortcut icon" href="{{ asset('/assets/img/opensid_logo.png') }}" />
+    <link rel="stylesheet" href="{{ asset('/assets/print/print.css') }}" />
+    <style type="text/css" media="print">
+        @page {size: landscape;}
+    </style>
+</head>
+
+<body>
+
+    <body>
+        <table>
+            <tbody>
+                <tr>
+                    <td class="padat">
+                        <img class="logo" src="{{ asset('/assets/img/opensid_logo.png') }}" alt="Logo">
+                        <h3 class="judul">PEMERINTAH <br /> KABUPATEN {{ config('app.namaKab') }}</h3>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <hr class="garis">
+                    </td>
+                </tr>
+                <tr>
+                    <td class="text-center">
+                        <h3> DATA PENDUDUK </h3>
+                    </td>
+                </tr>
+                <tr>
+                    <td>&nbsp;</td>
+                </tr>
+                <tr>
+                    <td>
+                        <table class="border thick" id="tabel-penduduk">
+                            <thead>
+                                <tr class="border thick">
+                                    <th>No</th>
+                                    <th>No. KK</th>
+                                    <th>NIK</th>
+                                    <th>Tag Id Card</th>
+                                    <th>Nama</th>
+                                    <th>Alamat</th>
+                                    <th>Dusun</th>
+                                    <th>RW</th>
+                                    <th>RT</th>
+                                    <th>Jenis Kelamin</th>
+                                    <th>Tempat Lahir</th>
+                                    <th>Tanggal Lahir</th>
+                                    <th>Umur</th>
+                                    <th>Agama</th>
+                                    <th>Pendidikan (dlm KK)</th>
+                                    <th>Pekerjaan</th>
+                                    <th>Kawin</th>
+                                    <th>Hub. Keluarga</th>
+                                    <th>Nama Ayah</th>
+                                    <th>Nama Ibu</th>
+                                    <th>Status</th>
+                                </tr>
+                            </thead>
+                            <tbody></tbody>
+                        </table>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+        <script src="{{ asset('/vendor/jquery/jquery.min.js') }}"></script>
+        <script>
+            $(document).ready(function() {
+                $.ajax({
+                    url: `{{ url("api/v1/penduduk") }}?{{ $filter }}`,
+                    method: 'get',
+                    success: function(json) {
+                        var no = 1;
+
+                        json.data.forEach(function(item) {
+                            var row = `
+                                <tr>
+                                    <td class="padat">${no}</td>
+                                    <td>${item.attributes.keluarga?.no_kk}</td>
+                                    <td>${item.attributes.nik}</td>
+                                    <td>${item.attributes.tag_id_card}</td>
+                                    <td>${item.attributes.nama}</td>
+                                    <td>${item.attributes.keluarga?.alamat}</td>
+                                    <td>${item.attributes.cluster_desa?.dusun}</td>
+                                    <td>${item.attributes.cluster_desa?.rw}</td>
+                                    <td>${item.attributes.cluster_desa?.rt}</td>
+                                    <td>${item.attributes.jenis_kelamin?.nama}</td>
+                                    <td>${item.attributes.tempatlahir}</td>
+                                    <td>${item.attributes.tanggalLahirId}</td>
+                                    <td>${item.attributes.umur}</td>
+                                    <td>${item.attributes.agama?.nama}</td>
+                                    <td>${item.attributes.pendidikan_k_k?.nama}</td>
+                                    <td>${item.attributes.pekerjaan?.nama}</td>
+                                    <td>${item.attributes.statusPerkawinan}</td>
+                                    <td>${item.attributes.penduduk_hubungan?.nama}</td>
+                                    <td>${item.attributes.nama_ayah}</td>
+                                    <td>${item.attributes.nama_ibu}</td>
+                                    <td>${item.attributes.penduduk_status?.nama}</td>
+                                </tr>
+                            `
+
+                            $('#tabel-penduduk tbody').append(row)
+                            no++;
+                        })
+                    }
+                })
+
+                $(document).ajaxStop(function() {
+                    window.print();
+                });
+
+                window.onafterprint = function() {
+                    window.close();
+                }
+            });
+        </script>
+    </body>
+
+</html>

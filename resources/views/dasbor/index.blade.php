601bb603b527655c70667fafd4ddb21219d948ea
FirlianiF <firlianif@gmail.com>
2023-04-30 03:21:05 +0700

    u

--- resources/views/dasbor/index.blade.php
+++ resources/views/dasbor/index.blade.php
@@ -88,7 +88,18 @@
                                             <label>Bulan</label>
                                             <select class="select2 form-control-sm" id="bulan" name="bulan"
                                                     data-placeholder="Semua Bulan" style="width: 100%;">
-                                                <option value="01"></option>
+                                                <option value="01">Januari</option>
+                                                <option value="02">Februari</option>
+                                                <option value="03">Maret</option>
+                                                <option value="04">April</option>
+                                                <option value="05">Mei</option>
+                                                <option value="06">Juni</option>
+                                                <option value="07">Juli</option>
+                                                <option value="08">Agustus</option>
+                                                <option value="09">September</option>
+                                                <option value="10">Oktober</option>
+                                                <option value="11">November</option>
+                                                <option value="12">Desember</option>
                                             </select>
                                         </div>
                                     </div>

5949736055d6c6d537c284e94f1111f2b086e9e0
Afila <apidongblog@gmail.com>
2023-05-08 00:12:35 +0700

    fronttend logo

--- resources/views/identitas/index.blade.php
+++ resources/views/identitas/index.blade.php
@@ -29,22 +29,13 @@
 @stop
 
 @section('content')
-    <div class="row" x-data="{
-        data: {},
 
-        async retrieveData() {
-            const response = await (await fetch('{{ url('api/v1/identitas') }}')).json();
-            this.data = response.data.attributes
-        }
-
-        unggahLogo (){
-
-        }
-    }" x-init="retrieveData">
+    <div class="row" x-data="identitas()" x-init="retrieveData()">
         <div class="col-lg-3 col-md-4">
             <div class="card card-widget">
                 <div class="widget-user-header text-center  p-4">
-                    <img :src="data.logo ?? '{{ asset('assets/img/opensid_logo.png') }}'" alt="Logo" width="150pxxx">
+                    <img :src="dataIdentitas.logo ?? '{{ asset('assets/img/opensid_logo.png') }}'" alt="Logo"
+                        width="150pxxx">
                     <h5 class="mt-3">Logo OpenKAB</h5>
                 </div>
 
@@ -52,7 +43,7 @@
                 <div class="card-footer">
                     <div class="row">
                         <div class="col-md-12">
-                            <button type="button" class="btn btn-block btn-primary btn-sm" id="ganti_logo">Ganti
+                            <button type="button" class="btn btn-block btn-primary btn-sm" @click="uploadGambar()">Ganti
                                 Logo</button>
                         </div>
                     </div>
@@ -77,62 +68,72 @@
             </div>
         </div>
     </div>
-@endsection
-
-@section('js')
     <script>
-        $(function() {
-            $('#ganti_logo').click(function(e) {
-                e.preventDefault();
-                Swal.fire({
-                    title: 'Unggah gambar',
-                    input: 'file',
-                    confirmButtonText: 'Unggah',
-                    showLoaderOnConfirm: true,
-                }).then((result) => {
-                    if (result.isConfirmed) {
-                        Swal.fire({
-                            title: 'Sedang Mengunggah',
-                            didOpen: () => {
-                                Swal.showLoading()
-                            },
-                            allowOutsideClick: false
-                        })
-                        var formData = new FormData();
-                        formData.append('file', result.value);
-                        $.ajax({
-                            url: '{{ url('api/v1/identitas/upload') }}',
-                            headers: {
-                                'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
-                            },
-                            type: 'POST',
-                            data: formData,
-                            processData: false, // tell jQuery not to process the data
-                            contentType: false, // tell jQuery not to set contentType
-                            success: function(response) {
-                                if (response.success == true) {
-                                    Swal.fire({
-                                        title: 'Simpan!',
-                                        text: 'Data berhasil tersimpan',
-                                        icon: 'success',
-                                        showConfirmButton: false,
-                                        timer: 1500,
-                                    })
-                                } else {
-                                    Swal.fire({
-                                        title: 'Error!',
-                                        text: response.message,
-                                        icon: 'error',
-                                        showConfirmButton: true,
-                                        allowOutsideClick: false
-                                    })
-                                }
-                            }
+        function identitas() {
+            return {
+                dataIdentitas: {},
+                retrieveData() {
+                    fetch('{{ url('api/v1/identitas') }}')
+                        .then(res => res.json())
+                        .then(response => {
+                            this.dataIdentitas = response.data.attributes;
+                            console.log(this.dataIdentitas)
                         });
-                    }
-                })
-            });
+                },
+                uploadGambar() {
+                    Swal.fire({
+                        title: 'Unggah gambar',
+                        input: 'file',
+                        confirmButtonText: 'Unggah',
+                        showLoaderOnConfirm: true,
+                    }).then((result) => {
+                        if (result.isConfirmed) {
+                            Swal.fire({
+                                title: 'Sedang Mengunggah',
+                                didOpen: () => {
+                                    Swal.showLoading()
+                                },
+                                allowOutsideClick: false
+                            })
+                            var formData = new FormData();
+                            formData.append('file', result.value);
+                            $.ajax({
+                                url: '{{ url('api/v1/identitas/upload') }}',
+                                headers: {
+                                    'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+                                },
+                                type: 'POST',
+                                data: formData,
+                                processData: false, // tell jQuery not to process the data
+                                contentType: false, // tell jQuery not to set contentType
+                                success: function(response) {
+                                    if (response.success == true) {
+                                        this.data.logo = response.data
+                                        Swal.fire({
+                                            title: 'Simpan!',
+                                            text: 'Data berhasil tersimpan',
+                                            icon: 'success',
+                                            showConfirmButton: false,
+                                            timer: 1500,
+                                        })
+                                        location.reload();
 
-        });
+                                    } else {
+                                        Swal.fire({
+                                            title: 'Error!',
+                                            text: response.message,
+                                            icon: 'error',
+                                            showConfirmButton: true,
+                                            allowOutsideClick: false
+                                        })
+                                    }
+                                }
+                            });
+                        }
+                    });
+                }
+            }
+        }
     </script>
 @endsection
+

21b05244f64f8dde90cf5f9e67151ca284263d5e
Andi Fahruddin Akas <andifahruddinakas@gmail.com>
2023-05-11 17:04:31 +0800

    dashboard_url

--- config/adminlte.php
+++ config/adminlte.php
@@ -253,7 +253,7 @@ return [
     */
 
     'use_route_url' => false,
-    'dashboard_url' => 'home',
+    'dashboard_url' => '/',
     'logout_url' => 'logout',
     'login_url' => 'login',
     'register_url' => false,

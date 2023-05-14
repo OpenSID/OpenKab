1aec004ed0c898fa0d894ecbcb06ababf0c103da
apidong <apidong@users.noreply.github.com>
2023-05-14 12:40:35 +0000

    Fix styling

--- app/Http/Controllers/Api/Auth/AuthController.php
+++ app/Http/Controllers/Api/Auth/AuthController.php
@@ -2,22 +2,16 @@
 
 namespace App\Http\Controllers\Api\Auth;
 
-use App\Models\User;
-use Illuminate\Support\Str;
-use Illuminate\Http\Request;
-use Illuminate\Auth\Events\Lockout;
 use App\Http\Controllers\Controller;
-use Illuminate\Support\Facades\Auth;
-use App\Http\Traits\LoginRequestTrait;
-use Illuminate\Support\Facades\Validator;
-use App\Http\Transformers\UserTransformer;
-use Illuminate\Support\Facades\RateLimiter;
-use App\Http\Transformers\PendudukMandiriTransformer;
-
-
-
+use App\Models\User;
+use Illuminate\Auth\Events\Lockout;
+use Illuminate\Http\Request;
 use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\RateLimiter;
+use Illuminate\Support\Str;
 use Illuminate\Validation\ValidationException;
+
 class AuthController extends Controller
 {
     /**
@@ -39,6 +33,7 @@ class AuthController extends Controller
      * Attempt to authenticate the request's credentials.
      *
      * @return void
+     *
      * @throws ValidationException
      */
     public function login(Request $request)
@@ -49,12 +44,13 @@ class AuthController extends Controller
             $seconds = RateLimiter::availableIn($this->throttleKey());
 
             return response()->json([
-                'message' => "USER TELAH DIBLOKIR KARENA GAGAL LOGIN ".static::MAX_ATTEMPT." KALI SILAKAN COBA KEMBALI DALAM 10 MENIT" ,
+                'message' => 'USER TELAH DIBLOKIR KARENA GAGAL LOGIN '.static::MAX_ATTEMPT.' KALI SILAKAN COBA KEMBALI DALAM 10 MENIT',
             ], Response::HTTP_FORBIDDEN);
         }
 
-        if (!Auth::attempt($request->only('email', 'password'))) {
+        if (! Auth::attempt($request->only('email', 'password'))) {
             RateLimiter::hit($this->throttleKey(), static::DECAY_SECOND);
+
             return response()->json([
                 'message' => 'Invalid login details',
             ], Response::HTTP_UNAUTHORIZED);
@@ -63,7 +59,7 @@ class AuthController extends Controller
         $user = User::where('email', $request['email'])->firstOrFail();
 
         // hapus token yang masih tersimpan
-        Auth::user()->tokens->each(function($token, $key) {
+        Auth::user()->tokens->each(function ($token, $key) {
             $token->delete();
         });
 
@@ -71,7 +67,7 @@ class AuthController extends Controller
         RateLimiter::clear($this->throttleKey());
 
         return response()
-            ->json(['message' => 'Login Success ', 'access_token' => $token, 'token_type' => 'Bearer',]);
+            ->json(['message' => 'Login Success ', 'access_token' => $token, 'token_type' => 'Bearer']);
     }
 
     /**
@@ -80,18 +76,19 @@ class AuthController extends Controller
     protected function logOut(Request $request)
     {
         auth()->user()->tokens()->delete();
+
         return response()->json([
-            'message' => "Berhasil Log Out" ,
+            'message' => 'Berhasil Log Out',
         ], Response::HTTP_OK);
     }
 
-     /**
+    /**
      * Get the rate limiting throttle key for the request.
      *
      * @return string
      */
     protected function throttleKey()
     {
-        return Str::lower(request('credential')) . '|' . request()->ip();
+        return Str::lower(request('credential')).'|'.request()->ip();
     }
 }

587685d0f57c5b527e57bbf1b76e84216fb77974
sarwono <scqolbu@gmail.com>
2023-07-04 05:51:02 +0700

    identitas

--- app/Http/Controllers/DasborController.php
+++ app/Http/Controllers/DasborController.php
@@ -2,15 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Identitas;
+
 class DasborController extends Controller
 {
     public function index()
     {
-        $identitas = Identitas::first();
-        $data['nama_aplikasi'] = $identitas->nama_aplikasi ?? config('app.namaAplikasi');
-        $data['sebutanKab'] = $identitas->sebutan_kab ?? config('app.sebutanKab');
-        $nama_kabupaten = preg_replace("/KAB/", "", $identitas->nama_kabupaten) ?? config('app.namaKab');
-        $data['nama_kabupaten'] = strtolower($data['sebutanKab']) == 'kota' ? $nama_kabupaten : $data['sebutanKab'] . ' ' . $nama_kabupaten;
+        $identitas = new Identitas();
+        $data = $identitas->pengaturan();
 
         return view('dasbor.index', compact('data'));
     }

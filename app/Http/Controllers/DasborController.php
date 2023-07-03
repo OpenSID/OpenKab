ef4422bc78c23f9b7efbaf066ddec0c16c0683d0
sarwono <scqolbu@gmail.com>
2023-07-03 22:18:23 +0700

    penamaan sebutan pada dasbor

--- app/Http/Controllers/DasborController.php
+++ app/Http/Controllers/DasborController.php
@@ -2,10 +2,18 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Identitas;
+
 class DasborController extends Controller
 {
     public function index()
     {
-        return view('dasbor.index');
+        $identitas = Identitas::first();
+        $data['nama_aplikasi'] = $identitas->nama_aplikasi ?? config('app.namaAplikasi');
+        $data['sebutanKab'] = $identitas->sebutan_kab ?? config('app.sebutanKab');
+        $nama_kabupaten = preg_replace("/KAB/", "", $identitas->nama_kabupaten) ?? config('app.namaKab');
+        $data['nama_kabupaten'] = strtolower($data['sebutanKab']) == 'kota' ? $nama_kabupaten : $data['sebutanKab'] . ' ' . $nama_kabupaten;
+
+        return view('dasbor.index', compact('data'));
     }
 }

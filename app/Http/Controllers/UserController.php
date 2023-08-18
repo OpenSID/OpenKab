c3ae9872403b198e4098d7b633efafa36b09d05d
ahmad afandi <ahmad.afandi85@gmail.com>
2023-08-18 08:51:13 +0700

    upload foto pengguna

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -9,6 +9,7 @@ use App\Models\User;
 use App\Models\UserTeam;
 use App\Traits\UploadedFile;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Auth;
 use Yajra\DataTables\DataTables;
 
@@ -176,14 +177,12 @@ class UserController extends Controller
     public function update(UserRequest $request, User $user)
     {
         try {
-            $data = $request->validated();
-
             $updateData = [
-                'name' => $data['name'],
-                'username' => $data['username'],
-                'email' => $data['email'],
-                'company' => $data['company'],
-                'phone' => $data['phone'],
+                'name' => $request->get('name'),
+                'username' => $request->get('username') ?? $user->username,
+                'email' => $request->get('email'),
+                'company' => $request->get('company'),
+                'phone' => $request->get('phone'),
             ];
             if($request->file('foto')){
                 $this->pathFolder .= '/profile';
@@ -191,8 +190,9 @@ class UserController extends Controller
             }
             $user->update($updateData);
 
-            if (! Auth::user()->isSuperAdmin()){
-                return redirect()->route('users.edit', Auth::id())->with('success', 'Data profil berhasil diubah!');
+            $routeCurrent = Route::currentRouteName();
+            if ( $routeCurrent == 'profile.update') {
+                return redirect()->route('profile.edit', Auth::id())->with('success', 'Data profil berhasil diubah!');
             }
 
             // update user team
--- resources/views/user/foto.blade.php
+++ resources/views/user/foto.blade.php
@@ -5,12 +5,15 @@
     </div>
     <div class="card-footer">
         <div class="input-group mb-3 px-2 py-2 bg-white shadow-sm">
-            <input id="upload" name="foto" type="file" onchange="readURL(this);" class="form-control border-0" style="display:none">
+            <input id="upload" name="foto" type="file" accept="image/*" class="form-control border-0 fade">
             <div class="input-group-append col-12">
                 <label for="upload" class="btn  col-12 btn-primary m-0 rounded-pill"> <i
                         class="fa fa-cloud-upload mr-2 text-muted"></i><small
                         class="text-uppercase text-white font-weight-bold text-muted">Ganti Foto</small></label>
             </div>
+            @error('foto')
+                <div class="text-danger">{{ $message }}</div>
+            @enderror
         </div>
     </div>
 </div>
@@ -31,7 +34,7 @@
 
     $(function () {
         $('#upload').on('change', function () {
-            readURL(input);
+            readURL(this);
         });
     });
     </script>

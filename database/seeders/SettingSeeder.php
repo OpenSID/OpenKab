49c5137fad9bce03c019110da0bf8f33f1bbd608
ahmad afandi <ahmad.afandi85@gmail.com>
2023-09-06 07:45:33 +0700

    Pengatuaran menampilkan statistik ke halaman public

--- database/seeders/SettingSeeder.php
+++ database/seeders/SettingSeeder.php
@@ -19,13 +19,28 @@ class SettingSeeder extends Seeder
             ['text' => 'Tidak Aktif', 'value' => 0],
             ['text' => 'Aktif', 'value' => 1],
         ];
-        Setting::firstOrCreate([
-            'key' => 'website_enable',
-            'name' => 'Pengaturan aktivasi website',
-            'value' => 1,
-            'type' => 'dropdown',
-            'attribute' => json_encode($attribute),
-            'description' => 'Pengaturan apakah website aktif atau tidak'
-        ]);
+        $website = Setting::where(['key' => 'website_enable'])->first();
+        if (!$website) {
+            Setting::create([
+                'key' => 'website_enable',
+                'name' => 'Pengaturan aktivasi website',
+                'value' => 1,
+                'type' => 'dropdown',
+                'attribute' => $attribute,
+                'description' => 'Pengaturan apakah website aktif atau tidak'
+            ]);
+        }
+
+        $statistik = Setting::where(['key' => 'statistik_website'])->first();
+        if (!$statistik) {
+            Setting::create([
+                'key' => 'statistik_website',
+                'name' => 'Pengaturan halaman statistik di website',
+                'value' => 1,
+                'type' => 'dropdown',
+                'attribute' => $attribute,
+                'description' => 'Pengaturan apakah statistik ditampilkan di website atau tidak'
+            ]);
+        }
     }
 }

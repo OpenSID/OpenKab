506ce8b0b495f5c9f904dc053c3bb5018d0ef6ca
ahmad afandi <ahmad.afandi85@gmail.com>
2023-09-25 09:35:26 +0700

    support multiple dropdown untuk menu

--- resources/js/web.js
+++ resources/js/web.js
@@ -1,7 +1,8 @@
 import $ from 'jquery';
 import '../vendor/bootstrap-5.3.2/js/bootstrap.bundle.min';
+import Bootnavbar from '../vendor/bootstrap-navbar-multi-level-dropdowns/js/bootnavbar';
 
 window.$ = $;
 window.jQuery = $;
 window.bootstrap = bootstrap;
-
+window.Bootnavbar = Bootnavbar;

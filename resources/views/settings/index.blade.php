59d8ef351248664bb58786e9d5ca5dbe8d5c69c9
Firliani Fauziah <firlianif@gmail.com>
2024-10-09 15:13:52 +0700

    sesuaikan hapus pengaturan

--- resources/views/settings/index.blade.php
+++ resources/views/settings/index.blade.php
@@ -82,16 +82,12 @@
                     {
                         data: function (data) {
                             let canEdit = `{{ $canedit }}`
-                            let canDelete = `{{ $candelete }}`
                             let buttonEdit = canEdit ? `<a href="{{ route('settings.index') }}/${data.id}/edit">
                                         <button type="button" class="btn btn-warning btn-sm edit" title="Ubah">
                                             <i class="fas fa-edit"></i>
                                         </button>
                                     </a>` : ``;
-                            let buttonDelete = canDelete ? `<button type="button" class="btn btn-danger btn-sm hapus" data-id="${data.id}" title="Hapus">
-                                    <i class="fas fa-trash"></i>
-                                </button>` : ``;
-                            return `${buttonEdit} ${buttonDelete}`;
+                            return `${buttonEdit}`;
                         },
                     },
                 ],

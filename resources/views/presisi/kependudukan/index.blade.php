70925415b8ae35e39377204a1d655a7b76a1010f
Firliani Fauziah <firlianif@gmail.com>
2024-10-21 16:00:03 +0700

    responsive

--- resources/views/presisi/kependudukan/index.blade.php
+++ resources/views/presisi/kependudukan/index.blade.php
@@ -11,7 +11,7 @@
             <div class="info-box shadow-none rounded-0">
                 <div class="info-box-content">
                     <div class="row">
-                        <div class="col-4">
+                        <div class="col-md-4">
                             <div class="card card-primary rounded-0 elevation-0 border">
                                 <div class="card-header rounded-0">
                                     <h3 class="card-title">Statistik Penduduk</h3>
@@ -35,7 +35,7 @@
 
                             </div>
                         </div>
-                        <div class="col-8">
+                        <div class="col-md-8">
                             <div class="card card-primary card-outline rounded-0 elevation-0 border">
                                 <div class="card-header">
                                     <div class="row">
@@ -46,14 +46,14 @@
                                                 <i class="fas fa-filter"></i>
                                             </a>
                                         </div>
-                                        <div class="col-2">
+                                        <div class="col-md-2">
                                             <button id="btn-tabel" class="btn btn-sm btn-info btn-block btn-sm "
                                                 data-bs-toggle="collapse" href="#Tabel" role="button"
                                                 aria-expanded="false" aria-controls="grafik-Tabel" disabled>
                                                 <i class="fas fa-chart-bar"></i> Tabel
                                             </button>
                                         </div>
-                                        <div class="col-2">
+                                        <div class="col-md-2">
                                             <button id="btn-grafik" class="btn btn-sm btn-success btn-block btn-sm"
                                                 data-bs-toggle="collapse" href="#grafik-statistik" role="button"
                                                 aria-expanded="false" aria-controls="grafik-statistik">

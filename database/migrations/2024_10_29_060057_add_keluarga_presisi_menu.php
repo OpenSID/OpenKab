7a606d62b77ce1433f73c8570c4d60886e936c91
Firliani Fauziah <firlianif@gmail.com>
2024-10-29 13:03:27 +0700

    migrasi data

---
+++ database/migrations/2024_10_29_060057_add_keluarga_presisi_menu.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        // Insert data baru tanpa tag <i> di kolom name
+        DB::table("menus")->where('name', 'Statistik Keluarga')->update(["url" => "/presisi/keluarga"]);
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+    }
+};

97d2fae37766ef9cc72b3679fc77887ab5c4609d
Firliani Fauziah <firlianif@gmail.com>
2024-10-30 13:33:02 +0700

    update

--- resources/views/presisi/bantuan/index.blade.php
+++ resources/views/presisi/bantuan/index.blade.php
@@ -98,36 +98,7 @@
                                             </select>
                                         </div>
                                     </div>
-                                    <div class="row">
-                                        <div class="col-1" style="display: none">
-                                            <a class="btn btn-sm btn-secondary" data-toggle="collapse"
-                                                href="#collapse-filter" role="button" aria-expanded="true"
-                                                aria-controls="collapse-filter">
-                                                <i class="fas fa-filter"></i>
-                                            </a>
-                                        </div>
-                                        <div style="display:none;" class="col-md-2">
-                                            <button id="btn-tabel" class="btn btn-sm btn-info btn-block btn-sm "
-                                                data-bs-toggle="collapse" href="#Tabel" role="button"
-                                                aria-expanded="false" aria-controls="grafik-Tabel" disabled>
-                                                <i class="fas fa-chart-bar"></i> Tabel
-                                            </button>
-                                        </div>
-                                        <div class="col-md-2">
-                                            <button id="btn-grafik" class="btn btn-sm btn-success btn-block btn-sm"
-                                                data-bs-toggle="collapse" href="#grafik-statistik" role="button"
-                                                aria-expanded="false" aria-controls="grafik-statistik" disabled>
-                                                <i class="fas fa-chart-bar"></i> Grafik
-                                            </button>
-                                        </div>
-                                        <div class="col-md-2">
-                                            <button id="btn-pie" class="btn btn-sm btn-warning btn-block btn-sm"
-                                                data-bs-toggle="collapse" href="#pie-statistik" role="button"
-                                                aria-expanded="false" aria-controls="pie-statistik">
-                                                <i class="fas fa-chart-pie"></i> Chart
-                                            </button>
-                                        </div>
-                                    </div>
+                                    
 
 
                                 </div>
@@ -163,13 +134,25 @@
                                     </div>
 
 
-                                    <div class="chart" id="grafik" style="height: 500px;">
-
+                                    @if(!$id)
+                                    <div class="row">
+                                        <div class="col-md-12">
+                                            <div class="chart" id="pie" style="height: 500px;"></div><br><br>
+                                        </div>
+                                        <div class="col-md-12">
+                                            <div class="chart" id="grafik" style="height: 500px;"></div>
+                                        </div>
                                     </div>
-
-                                    <div class="chart" id="pie" style="height: 500px; display: none">
-
+                                    @else
+                                    <div class="row">
+                                        <div class="col-md-8">
+                                            <div class="chart" id="grafik" style="height: 500px;"></div>
+                                        </div>
+                                        <div class="col-md-4">
+                                            <div class="chart" id="pie" style="height: 500px;"></div>
+                                        </div>
                                     </div>
+                                    @endif
                                     <br/>
                                     <h5 class="pl-2"> Tabel Penerima Bantuan</h5>
                                     <div class="table-responsive mailbox-messages">

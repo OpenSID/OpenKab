d4a94d8be24f6e779cb136d02459764db002ce43
Firliani Fauziah <firlianif@gmail.com>
2024-10-30 13:00:58 +0700

    update

---
+++ resources/views/presisi/bantuan/list_program.blade.php
@@ -0,0 +1,48 @@
+function GetListProgram() {
+    $('#filter_program').empty().trigger("change");
+    @if($id) 
+        @if($id == 'penduduk')
+            var optionPenduduk = new Option("Penerima Bantuan Penduduk", "penduduk");
+            $("#filter_program").append(optionPenduduk);
+        @elseif($id == 'keluarga')
+            var optionKeluarga = new Option("Penerima Bantuan Keluarga", "keluarga");
+            $("#filter_program").append(optionKeluarga);
+        @else
+            $.ajax({
+                type: 'GET',
+                url: "{{ url('api/v1/statistik-web/get-list-program') }}",
+                dataType: 'json',
+                success: function(data) {
+                    for (var i = 0; i < data.length; i++) {
+                        var newOption = new Option(data[i].nama, data[i].id, true, true);
+                        $("#filter_program").append(newOption);
+                    }
+                    $("#filter_program").val("penduduk");
+                },
+                error: function(jqXHR, textStatus, errorThrown) {
+
+                }
+            });
+        @endif
+    @else
+        var optionPenduduk = new Option("Penerima Bantuan Penduduk", "penduduk");
+        $("#filter_program").append(optionPenduduk);
+        var optionKeluarga = new Option("Penerima Bantuan Keluarga", "keluarga");
+        $("#filter_program").append(optionKeluarga);
+        $.ajax({
+            type: 'GET',
+            url: "{{ url('api/v1/statistik-web/get-list-program') }}",
+            dataType: 'json',
+            success: function(data) {
+                for (var i = 0; i < data.length; i++) {
+                    var newOption = new Option(data[i].nama, data[i].id, true, true);
+                    $("#filter_program").append(newOption);
+                }
+                $("#filter_program").val("penduduk");
+            },
+            error: function(jqXHR, textStatus, errorThrown) {
+
+            }
+        });
+    @endif
+}

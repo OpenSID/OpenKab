2e4717440b0d74f0fbaf877478a83a5f79a5bc4c
misarianto <amikrian8@gmail.com>
2024-11-24 16:23:42 +0700

    REMOVE: menghapus comment pada kode

--- app/Http/Controllers/DataPokokController.php
+++ app/Http/Controllers/DataPokokController.php
@@ -6,7 +6,6 @@ class DataPokokController extends Controller
 {
     public function pendidikan()
     {
-        // dd(session('kabupaten.kode_kabupaten'),session('kecamatan.kode_kecamatan'), session('desa.id'), session()->all());
         return view('pendidikan.index');
     }
 }

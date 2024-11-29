b3c2c9ca763f9785dd19737f9494118e2e0c4f59
misarianto <misarianto@users.noreply.github.com>
2024-11-29 12:34:38 +0000

    Fix styling

--- app/Http/Controllers/DataPokokController.php
+++ app/Http/Controllers/DataPokokController.php
@@ -7,6 +7,7 @@ class DataPokokController extends Controller
     public function jaminanSosial()
     {
         $title = 'Data Kepesertaan Program dan Statistik';
+
         return view('jaminan_sosial.index', ['title' => $title]);
     }
 }
--- app/Http/Repository/KelembagaanRepository.php
+++ app/Http/Repository/KelembagaanRepository.php
@@ -3,8 +3,8 @@
 namespace App\Http\Repository;
 
 use App\Models\Potensi;
-use Spatie\QueryBuilder\QueryBuilder;
 use Spatie\QueryBuilder\AllowedFilter;
+use Spatie\QueryBuilder\QueryBuilder;
 
 class KelembagaanRepository
 {

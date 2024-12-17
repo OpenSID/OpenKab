25518a5e48c3884bda90457fef037396952ddb4e
Firliani Fauziah <firlianif@gmail.com>
2024-12-17 14:52:32 +0700

    update

---
+++ resources/views/data_pokok/infrastruktur/style.blade.php
@@ -0,0 +1,16 @@
+@push('css')
+<style nonce="{{ csp_nonce() }}" >
+        #kondisiChart {
+            min-height: 250px!important;
+            height: 250px!important;
+            max-height: 250px!important;
+            max-width: 100%;
+        }
+        #sanitasiChart {
+            min-height: 250px!important;
+            height: 250px!important;
+            max-height: 250px!important;
+            max-width: 100%;
+        }
+    </style>
+@endpush

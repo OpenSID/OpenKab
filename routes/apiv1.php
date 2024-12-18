45d5b9ffe3d41501e50d779045baef84f266bf34
Firliani Fauziah <firlianif@gmail.com>
2024-12-18 10:57:01 +0700

    update

--- routes/apiv1.php
+++ routes/apiv1.php
@@ -28,7 +28,6 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\PrasaranaSaranaController;
 use App\Http\Controllers\Api\BantuanKabupatenController;
 use App\Http\Controllers\Api\KelembagaanController;
-use App\Http\Controllers\Api\InfrastrukturController;
 
 /*
 |--------------------------------------------------------------------------
@@ -86,8 +85,6 @@ Route::middleware(['auth:sanctum'])->group(function () {
     });
 
     Route::get('/pariwisata', PariwisataController::class);
-
-    Route::get('infrastruktur', [InfrastrukturController::class, 'data']);
 
     // API Data Presisi
     Route::get('/ketenagakerjaan', KetenagakerjaanController::class);

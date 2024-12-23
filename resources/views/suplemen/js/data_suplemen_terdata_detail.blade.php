67030b5cb568b21ba76e0fcae8f84cccc7385b94
Firliani Fauziah <firlianif@gmail.com>
2024-12-23 11:49:45 +0700

    u

--- resources/views/suplemen/js/data_suplemen_terdata_detail.blade.php
+++ resources/views/suplemen/js/data_suplemen_terdata_detail.blade.php
@@ -13,29 +13,6 @@ $('#suplemen').on('click', '.btn-details', function() {
 
     var button = $(this);
 
-    // Cek apakah baris sudah terbuka atau belum
-    if (row.child.isShown()) {
-        // Jika sudah terbuka, sembunyikan
-        row.child.hide();
-        tr.removeClass('shown');
-        
-        // Ubah tombol menjadi "Selengkapnya" dan ubah warna ke hijau
-        button.text('Selengkapnya');
-        button.removeClass('btn-danger');
-        button.addClass('btn-success');
-    } else {
-        // Jika belum terbuka, tampilkan detail
-        var detailHtml = '';
-        $.each(jsonData, function(key, value) {
-            detailHtml += `<p><b>${key}</b>: ${value}</p>`;
-        });
+    @include('suplemen.js.button_validation')
 
-        row.child(detailHtml).show();
-        tr.addClass('shown');
-
-        // Ubah tombol menjadi "Tutup" dan ubah warna ke merah
-        button.text('Tutup');
-        button.removeClass('btn-success');
-        button.addClass('btn-danger');
-    }
 });

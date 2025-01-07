52327b69ae7b1151f315c0f31d55870eae5efdb7
misarianto <amikrian8@gmail.com>
2025-01-07 09:12:40 +0700

    UPDATE: mengembalikan permission dan route ke sebelumnya

--- resources/views/penduduk/index.blade.php
+++ resources/views/penduduk/index.blade.php
@@ -210,7 +210,7 @@
                         var pindah = (data.attributes.status_dasar == 1) ? '' : 'disabled';
                         let canEdit = `{{ $canedit }}`
                         let pindahMenu = canEdit ? `<li>
-                                    <a href="${'{{ route("profile.kependudukan.penduduk.pindah", ["id" => "ID"]) }}'.replace('ID', data.id)}" class="btn btn-social btn-flat btn-block btn-sm ${pindah} "><i class="fas fa-exchange-alt"></i> Pindah Penduduk</a>
+                                    <a href="{{ url('penduduk/pindah') }}/${data.id}" class="btn btn-social btn-flat btn-block btn-sm ${pindah} "><i class="fas fa-exchange-alt"></i> Pindah Penduduk</a>
                                 </li>` : ``;
                         return `<div class="btn-group open">
                             <button type="button" class="btn btn-social btn-flat btn-info btn-sm" data-toggle="dropdown" aria-expanded="true"><i class="fa fa-arrow-circle-down"></i> Pilih Aksi</button>
@@ -343,7 +343,7 @@
         });
 
         $('#cetak').on('click', function() {
-            window.open(`{{ route('profile.kependudukan.penduduk.cetak') }}?${$.param(penduduk.ajax.params())}`, '_blank');
+            window.open(`{{ url('penduduk/cetak') }}?${$.param(penduduk.ajax.params())}`, '_blank');
         });
     });
     </script>

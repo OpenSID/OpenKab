c7e0ac098ec3b29d6e99190f2dd810eafa22ce76
ahmad afandi <ahmad.afandi85@gmail.com>
2025-01-20 17:13:18 +0700

    hapus code yang tidak diperlukan

--- app/Http/Controllers/OpenDKController.php
+++ app/Http/Controllers/OpenDKController.php
@@ -2,25 +2,15 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Repository\SettingRepository;
-use App\Http\Requests\CreateSettingRequest;
-use App\Http\Requests\UpdateSettingRequest;
-use App\Http\Transformers\SettingTransformer;
-use App\Models\Setting;
-use App\Models\SettingModul;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Session;
 
 class OpenDKController extends AppBaseController
 {
-    /** @var SettingRepository */
-    private $settingRepository;
-
     protected $permission = 'pengaturan-opendk';
 
-    public function __construct(SettingRepository $settingRepo)
+    public function __construct()
     {
-        $this->settingRepository = $settingRepo;
+        parent::__construct();
     }
 
     /**
@@ -28,47 +18,10 @@ class OpenDKController extends AppBaseController
      */
     public function index(Request $request)
     {
-        if ($request->ajax()) {
-            return $this->fractal($this->settingRepository->listSetting(), new SettingTransformer, 'settings')->respond();
-        }
-
         $listPermission = $this->generateListPermission();
 
         return view('opendk.index')->with($listPermission);
-    }    
-        
-
-    /**
-     * Update the specified Setting in storage.
-     */
-    public function update($id, UpdateSettingRequest $request)
-    {
-        if (empty(SettingModul::where('url', 'like', '%prodeskel%')->first()) && $request->value == 'presisi') {
-            $setting = Setting::where('key', 'home_page')->where('value', 'presisi')->first();
-
-            if ($setting) {
-                $setting->update(['value' => 'default']);
-            }
-
-            Session::flash('alert', 'Pengaturan halaman utama dasbor presisi tidak dapat diaktifkan, mohon pastikan prodeskel pada OpenSID harus terpasang.');
-
-            return redirect(route('opendk.index'));
-        }
-
-        $setting = $this->settingRepository->find($id);
-
-        if (empty($setting)) {
-            Session::flash('error', 'Setting tidak ditemukan');
-
-            return redirect(route('opendk.index'));
-        }
-
-        $setting = $this->settingRepository->update($request->all(), $id);
-
-        Session::flash('success', 'Setting berhasil diupdate.');
-
-        return redirect(route('opendk.index'));
-    }    
+    }
 
     protected function getOptionItems($id = null)
     {

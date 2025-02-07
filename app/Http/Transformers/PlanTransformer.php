77de777a5aeee7a0823faab53de419e65dc09cac
FirlianiF <firlianif@gmail.com>
2025-02-07 16:21:42 +0700

    update

--- app/Http/Transformers/PlanTransformer.php
+++ app/Http/Transformers/PlanTransformer.php
@@ -25,8 +25,10 @@ class PlanTransformer extends TransformerAbstract
      */
     protected function generateAksiColumn(Lokasi $plan): string
     {
-        $aksi = '<a href="'.url('plan/ajax_lokasi_maps/'.$plan->point->parent->id.'/'.$plan->id).'" class="btn bg-olive btn-sm" title="Lokasi '.$plan->nama.'"><i class="fa fa-map"></i></a> ';
-
-        return $aksi;
+        if($plan->point->parent){
+            $aksi = '<a href="'.url('plan/ajax_lokasi_maps/'.$plan->point->parent->id.'/'.$plan->id).'" class="btn bg-olive btn-sm" title="Lokasi '.$plan->nama.'"><i class="fa fa-map"></i></a> ';
+        }
+        
+        return $aksi ?? '';
     }
 }

5aa7d5998e014a1612df97cfd0e7298ad2351a4a
FirlianiF <firlianif@gmail.com>
2025-02-25 14:16:20 +0700

    data ketenagakerjaan

--- resources/views/data_pokok/ketenagakerjaan/index.blade.php
+++ resources/views/data_pokok/ketenagakerjaan/index.blade.php
@@ -2,7 +2,7 @@
 
 @include('components.progressive-image')
 
-@section('title', 'Data Bantuan')
+@section('title', 'Data Pekerjaan dan Pelatihan')
 
 @section('content_header')
     <h1>{{ $title }}</h1>
@@ -49,7 +49,7 @@
                         <table class="table table-striped" id="ketenagakerjaan">
                             <thead>
                                 <tr>
-                                    <th>ID</th>
+                                    <th>No</th>
                                     <th>NIK</th>
                                     <th>Pekerjaan</th>
                                     <th>Jabatan</th>
@@ -80,7 +80,7 @@
             processing: true,
             serverSide: true,
             autoWidth: false,
-            ordering: true,
+            ordering: false,
             searchPanes: {
                 viewTotal: false,
                 columns: [0]
@@ -93,8 +93,6 @@
                         "page[size]": row.length,
                         "page[number]": (row.start / row.length) + 1,
                         "filter[search]": row.search.value,
-                        "sort": (row.order[0]?.dir === "asc" ? "" : "-") + row.columns[row.order[0]?.column]
-                            ?.name,
                         "filter[kode_desa]": $("#kode_desa").val(),
                     };
                 },
@@ -120,6 +118,7 @@
                 ],
             columns: [{
                     data: null,
+                    orderable: false
                 },
                 {
                     data: "attributes.nik",
@@ -147,9 +146,6 @@
                     orderable: false
                 },
             ],
-            order: [
-                [0, 'asc']
-            ]
         })
         ketenagakerjaan.on('draw.dt', function() {
             var PageInfo = $('#ketenagakerjaan').DataTable().page.info();

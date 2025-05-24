e438f1fd69137bc08ddecfb977d00b60ee195efc
misarianto <amikrian8@gmail.com>
2025-05-24 19:58:16 +0700

    UPDATE: perbaikan filter kesehatan

--- resources/views/presisi/kesehatan/filter.blade.php
+++ resources/views/presisi/kesehatan/filter.blade.php
@@ -26,10 +26,9 @@
         <select name="id" id="id" required class="form-control input-sm" title="Pilih salah satu">
             <option value="null">Posyandu</option>
             @foreach ($data['posyandu'] as $item)
-                <option value="{{ $item['id'] }}" {{ $item['id'] == $data['id'] ? 'selected' : '' }}>
-                    {{ $item['attributes']['nama'] }}</option>
+                <option value="{{ $item['id'] }}" {{ $item['id'] == $id ? 'selected' : '' }}>
+                    {{ $item['nama'] }}</option>
             @endforeach
         </select>
     </div>
-</div>
 </div>

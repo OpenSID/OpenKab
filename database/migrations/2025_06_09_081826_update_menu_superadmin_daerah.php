7157b740642a4c242abddf62696809de9d13f89c
misarianto <amikrian8@gmail.com>
2025-06-09 15:30:04 +0700

    UPDATE: set menu kecuali menu pengaturan group

---
+++ database/migrations/2025_06_09_081826_update_menu_superadmin_daerah.php
@@ -0,0 +1,53 @@
+<?php
+
+use App\Enums\Modul;
+use App\Models\Team;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        $team = Team::where('name', 'superadmin_daerah')->first();
+
+        if (!$team) return;
+
+        // update ke [] dulu, agar tidak berntrok jika sudah di set manual
+        $team->update(['menu' => []]);
+
+        // baru update yang baru tanpa pengaturan group
+        $filteredMenu = array_map(function ($item) {
+            if (!isset($item['submenu'])) return $item;
+
+            $item['submenu'] = array_filter($item['submenu'], function ($submenuItem) {
+                return $submenuItem['url'] !== 'pengaturan/groups';
+            });
+
+            return $item;
+        }, Modul::Menu);
+
+        $team->update(['menu' => $filteredMenu]);
+
+
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        $team = Team::where('name', 'superadmin_daerah')->first();
+        if ($team) {
+            $team->update(['menu' => Modul::Menu]);
+        }
+    }
+};

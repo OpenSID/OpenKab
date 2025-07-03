c2acf2632e697db126f0cf23ae25db37df99eeb4
pandigresik <pandigresik@users.noreply.github.com>
2025-07-03 09:07:35 +0000

    Fix styling

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,7 +4,6 @@ namespace App\Providers;
 
 use App\Http\Transformers\IdentitasTransformer;
 use App\Http\Transformers\SettingTransformer;
-use App\Models\Config;
 use App\Models\Identitas;
 use App\Models\Setting;
 use Illuminate\Support\Facades\App;

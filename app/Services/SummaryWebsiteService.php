d484922cec6ecd6b7a866d19e9eac9b044c3e611
ahmad afandi <ahmad.afandi85@gmail.com>
2025-07-07 11:32:52 +0700

    Perbaikan statistik desa pada halaman website

--- app/Services/SummaryWebsiteService.php
+++ app/Services/SummaryWebsiteService.php
@@ -6,13 +6,16 @@ use Illuminate\Support\Facades\Cache;
 
 class SummaryWebsiteService extends BaseApiService
 {
-    public function getSummaryData()
+    public function getSummaryData(array $filters = [])
     {
         $cacheKey = 'summary_website_data';
-
+        if(!empty($filters)) {
+            // Tambahkan filter ke cache key untuk menghindari konflik cache
+            $cacheKey .= '_' . md5(json_encode($filters));
+        }
         // Cek cache terlebih dahulu
-        return Cache::remember($cacheKey, now()->addHours(6), function () {
-            return $this->apiRequest('/api/v1/data-website');
+        return Cache::remember($cacheKey, now()->addHours(6), function () use ($filters) {
+            return $this->apiRequest('/api/v1/data-website', $filters);
         });
     }
 }

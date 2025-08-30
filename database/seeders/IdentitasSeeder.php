0d863f276f43b862fe2225fe01b8de89cf63dc11
ahmad afandi <ahmad.afandi85@gmail.com>
2025-08-30 10:20:20 +0700

    Perbaikan install baru

--- database/seeders/IdentitasSeeder.php
+++ database/seeders/IdentitasSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use App\Models\Identitas;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Schema;
 
 class IdentitasSeeder extends Seeder
 {
@@ -14,14 +15,17 @@ class IdentitasSeeder extends Seeder
      */
     public function run()
     {
-        return Identitas::create([
+        $defaultData = [
             'nama_aplikasi' => 'Simatik',
             'nama_kabupaten' => 'KOTA BIMA',
             'kode_kabupaten' => '52.72',
             'nama_provinsi' => 'Nusa Tenggara Barat',
             'kode_provinsi' => '52',
-            'sebutan_kab' => 'Kota',
-            'sebutan_desa' => 'Kelurahan',
-        ]);
+            'sebutan_kab' => 'Kota'
+        ];
+        if(Schema::hasColumn('identitas', 'sebutan_desa')){
+            $defaultData['sebutan_desa'] = 'Kelurahan';
+        }
+        return Identitas::create($defaultData);
     }
 }

0197217562ae5c4773a1916f9b3c588e55176eaf
habibie11 <habibie.mahbub@gmail.com>
2025-11-25 21:45:41 +0700

    statistik pangan

--- routes/web.php
+++ routes/web.php
@@ -319,6 +319,10 @@ Route::middleware(['auth', 'teams_permission', 'password.weak', '2fa'])->group(f
             Route::get('cetak', [App\Http\Controllers\DataPresisiAdatController::class, 'cetak'])->name('data-pokok.data-presisi-adat.cetak');
         })
         ->middleware(['permission:datapresisi-adat-read']);
+
+        Route::prefix('statistik')->group(function () {
+            Route::get('pangan', [App\Http\Controllers\StatistikPanganController::class, 'index']); 
+        });
     });
 
     // Prodeskel

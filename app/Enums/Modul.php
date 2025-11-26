e6adf168c07ce2da601884992d21ba9d11c45625
habibie11 <habibie.mahbub@gmail.com>
2025-11-26 14:17:38 +0700

    statistik pengisian datapresisi kesehatan

--- app/Enums/Modul.php
+++ app/Enums/Modul.php
@@ -106,6 +106,18 @@ final class Modul extends Enum
             'permission' => 'datapresisi',
             'submenu' => [
                 [
+                    'text' => 'Statistik',
+                    'icon' => 'fa fa-chart-pie',
+                    'submenu' => [
+                        [
+                            'icon' => 'far fa-fw fa-circle',
+                            'text' => 'Kesehatan',
+                            'url' => 'data-presisi/statistik/kesehatan',
+                            'permission' => 'statistik-datapresisi-kesehatan',
+                        ],
+                    ]
+                ],
+                [
                     'icon' => 'far fa-fw fa-circle',
                     'text' => 'Data Presisi Pangan',
                     'url' => 'data-presisi/pangan',

42cf1d466b14a5abfee74507ed469eb3d76cd75c
Ahmad Afandi <ahmad.afandi85@gmail.com>
2026-04-27 13:38:38 +0700

    feat: detail statistik pendidikan

--- app/Http/Controllers/DataPresisiPendidikanController.php
+++ app/Http/Controllers/DataPresisiPendidikanController.php
@@ -3,24 +3,62 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\View\View;
 
 class DataPresisiPendidikanController extends Controller
 {
-    public function index()
+    /**
+     * Show the data presisi pendidikan index page.
+     *
+     * @return \Illuminate\View\View
+     */
+    public function index(): View
     {
         $title = 'Data Presisi Pendidikan';
 
         return view('data_pokok.data_presisi.pendidikan.index', compact('title'));
     }
 
-    public function detail(Request $request)
+    /**
+     * Display detail data presisi pendidikan.
+     *
+     * @param Request $request HTTP request instance
+     * @return View
+     */
+    public function detail(Request $request): View
     {
         $data = json_decode($request->data);
 
         return view('data_pokok.data_presisi.pendidikan.detail', ['data' => $data]);
     }
 
-    public function cetak(Request $request)
+    /**
+     * Display detail data presisi pendidikan with optional filter.
+     *
+     * @param Request $request HTTP request instance
+     * @return View
+     */
+    public function detailData(Request $request): View
+    {
+        $colomn = '';
+        $title = 'Data Presisi Pendidikan - '.$request->input('judul');
+        $title = htmlspecialchars(strip_tags($title), ENT_QUOTES, 'UTF-8');
+        $filter = $request->input('filter');
+
+        if (isset($filter['tipe'], $filter['nilai']) && $filter['tipe'] && $filter['nilai']) {
+            $colomn = $filter['tipe'].':'.$filter['nilai'];
+        }
+
+        return view('data_pokok.data_presisi.pendidikan.detail_data', compact('title', 'colomn'));
+    }
+
+    /**
+     * Display print view for data presisi pendidikan.
+     *
+     * @param Request $request HTTP request instance
+     * @return View
+     */
+    public function cetak(Request $request): View
     {
         return view('data_pokok.data_presisi.pendidikan.cetak', ['filter' => $request->getQueryString()]);
     }
---
+++ tests/Feature/DataPresisiPendidikanControllerTest.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Http\Controllers\DataPresisiPendidikanController;
+use Illuminate\Http\Request;
+use Illuminate\View\View;
+use Tests\BaseTestCase;
+
+class DataPresisiPendidikanControllerTest extends BaseTestCase
+{
+    private DataPresisiPendidikanController $controller;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+        $this->controller = new DataPresisiPendidikanController;
+    }
+
+    public function test_detail_data_without_filter_returns_correct_view(): void
+    {
+        $response = $this->controller->detailData(new Request());
+
+        $this->assertInstanceOf(View::class, $response);
+        $this->assertEquals('data_pokok.data_presisi.pendidikan.detail_data', $response->name());
+
+        $data = $response->getData();
+        $this->assertArrayHasKey('title', $data);
+        $this->assertArrayHasKey('colomn', $data);
+        $this->assertEquals('Data Presisi Pendidikan - ', $data['title']);
+        $this->assertEquals('', $data['colomn']);
+    }
+
+    public function test_detail_data_with_filter_returns_correct_view(): void
+    {
+        $response = $this->controller->detailData(new Request([
+            'judul' => 'Test Judul',
+            'filter' => [
+                'tipe' => 'kategori',
+                'nilai' => 'pendidikan',
+            ],
+        ]));
+
+        $this->assertInstanceOf(View::class, $response);
+
+        $data = $response->getData();
+        $this->assertEquals('Data Presisi Pendidikan - Test Judul', $data['title']);
+        $this->assertEquals('kategori:pendidikan', $data['colomn']);
+    }
+
+    public function test_detail_data_with_partial_filter_returns_empty_colomn(): void
+    {
+        request()->merge([
+            'judul' => 'Test Judul',
+            'filter' => [
+                'tipe' => 'kategori',
+            ],
+        ]);
+
+        $response = $this->controller->detailData(new Request());
+
+        $data = $response->getData();
+        $this->assertEquals('', $data['colomn']);
+    }
+
+    public function test_detail_data_title_is_sanitized_against_xss(): void
+    {
+        $response = $this->controller->detailData(new Request([
+            'judul' => '<script>alert("xss")</script>Test',
+        ]));
+
+        $data = $response->getData();
+        $this->assertStringNotContainsString('<script>', $data['title']);
+        $this->assertStringContainsString('Test', $data['title']);
+    }
+}

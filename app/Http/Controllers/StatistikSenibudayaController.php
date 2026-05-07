0c134b8b06ac105a1fcc5b168d877df9d0dbdb5e
Ahmad Afandi <ahmad.afandi85@gmail.com>
2026-05-07 09:45:32 +0700

    feat: detail statistik seni

--- app/Http/Controllers/StatistikSenibudayaController.php
+++ app/Http/Controllers/StatistikSenibudayaController.php
@@ -2,13 +2,23 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\View\View;
+
+/**
+ * Controller untuk Statistik Seni Budaya
+ *
+ * Menampilkan data statistik seni budaya dengan link ke detail data
+ */
 class StatistikSenibudayaController extends Controller
 {
-    public function index()
+    /**
+     * Menampilkan halaman statistik seni budaya
+     */
+    public function index(): View
     {
         return view('presisi.statistik.senibudaya', [
-            'detailLink' => url(''),
-            'judul' => 'seni budaya'            
+            'detailLink' => url('data-presisi/seni-budaya/detail_data'),
+            'judul' => 'seni budaya',
         ]);
-    }    
+    }
 }

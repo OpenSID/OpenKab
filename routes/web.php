edeb893e3d11ab31df44829bba170ac709f958fd
Ahmad Afandi <ahmad.afandi85@gmail.com>
2026-05-07 14:20:06 +0700

    fix: gambar logo desa pada website desa aktif

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AdminWebController;
+use App\Http\Controllers\ImageProxyController;
 use App\Http\Controllers\Web\ArtikelController;
 use App\Http\Controllers\ApiProxyController;
 use App\Http\Controllers\Auth\ChangePasswordController;
@@ -455,3 +456,6 @@ Route::get('/module/keluarga/{id}', [PresisiController::class, 'keluarga'])->nam
 Route::get('/statistik-keluarga', [PresisiController::class, 'keluarga'])->name('presisi.keluarga');
 Route::get('/module/kesehatan/{id}', [PresisiController::class, 'kesehatan'])->name('presisi.kesehatan');
 Route::get('/statistik-kesehatan', [PresisiController::class, 'kesehatan'])->name('presisi.kesehatan');
+
+// Image proxy route
+Route::get('/image-proxy', [ImageProxyController::class, 'proxy'])->name('image.proxy');

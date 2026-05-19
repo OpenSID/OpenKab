4b32296aadeb7dee92eaf15b1537c522aed69a82
Ahmad Afandi <ahmad.afandi85@gmail.com>
2026-05-19 14:07:06 +0700

    fix: perbaikan tampilan website ketika slider belum diisi

--- resources/views/web/partials/slider.blade.php
+++ resources/views/web/partials/slider.blade.php
@@ -1,10 +1,16 @@
 <div class="col-md-12 animated fadeIn">
     <div class="owl-carousel header-carousel">
+        @php
+            $placeholderImage = asset('assets/img/no-image.png');
+        @endphp
         @forelse ((new App\Http\Repository\CMS\SlideRepository)->activeSlide(5) as $slide)
             <div class="owl-carousel-item">
-                <img class="img-fluid" src="{{ Storage::url($slide->thumbnail) }}" alt="">
+                <img class="img-fluid" src="{{ Storage::url($slide->thumbnail) }}" alt="" onerror="this.onerror=null;this.src='{{ $placeholderImage }}';">
             </div>
         @empty
+            <div class="owl-carousel-item">
+                <img class="img-fluid" src="{{ $placeholderImage }}" alt="No Slide Available">
+            </div>
         @endforelse
     </div>
 </div>

c7a1bef65e791f6ecf1cb5985f87df703e4b8503
Ahmad Afandi <ahmad.afandi85@gmail.com>
2026-05-19 15:35:37 +0700

    feat: datatable debounce

--- resources/views/layouts/index.blade.php
+++ resources/views/layouts/index.blade.php
@@ -69,7 +69,36 @@ Seluruh hak cipta dilindungi.
         $.extend($.fn.dataTable.defaults, {
             language: {
                 url: "{{ asset('vendor/datatable/id.json') }}"
-            }
+            },
+            searchDelay: 500,
+        });
+
+        $(document).on('init.dt', function(e, settings) {
+            if (e.namespace !== 'dt') return;
+
+            var table = new $.fn.dataTable.Api(settings);
+            var searchDelay = table.init().searchDelay || 1500;
+            var searchInput = $('div.dataTables_filter input', table.table().container());
+            var debounceTimer = null;
+            var previousSearch = null;
+
+            searchInput.off('keyup.DT input.DT search.DT keydown.DT');
+
+            searchInput.on('keyup input', function() {
+                var currentValue = this.value;
+
+                if (previousSearch === currentValue) return;
+
+                previousSearch = currentValue;
+
+                clearTimeout(debounceTimer);
+
+                debounceTimer = setTimeout(function() {
+                    if (table.search() !== currentValue) {
+                        table.search(currentValue).draw();
+                    }
+                }, searchDelay);
+            });
         });
 
 

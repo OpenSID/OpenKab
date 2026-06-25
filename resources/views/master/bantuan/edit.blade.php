3aaf6381bb36419098a649d33e543e4d70cc9704
Ahmad Afandi <ahmad.afandi85@gmail.com>
2026-06-25 13:18:31 +0700

    sederhanakan code

--- resources/views/master/bantuan/edit.blade.php
+++ resources/views/master/bantuan/edit.blade.php
@@ -1,18 +1,6 @@
 @extends('layouts.index')
 
-@push('css')
-    <style nonce="{{ csp_nonce() }}">
-       .select2-container--default .select2-selection--multiple .select2-selection__choice{
-        background-color: #007bff;
-        border-color: #007bff;
-        color: #ffffff;
-       }
-       .select2-container--default .select2-selection--multiple .select2-selection__choice__remove{
-        border-right: none;
-        color: #FFFFFF
-       }       
-    </style>
-@endpush
+@include('partials.select2_multi_select')
 
 @section('title', 'Ubah Bantuan')
 
